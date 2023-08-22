d83da64b35dd1877485206f98e9358f05fea3fe4
michellemwangi01 <michellemwangim@gmail.com>
2023-08-22 22:44:17 +0300

    completed

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,18 @@
 
 from user import User
 
+
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        if knowledge is None:
+            self.knowledge = []
+        else:
+            self.knowledge = knowledge
+
+    def learn(self, lesson):
+        self.knowledge.append(lesson)
+
+
+nwestu = Student("fname", "lname", [])
+nwestu.first_name
